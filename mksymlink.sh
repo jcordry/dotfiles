e98eb1f817d81392d6d9a2871f16e29ed44c20f2
Julien Cordry <julien.cordry@gmail.com>
2014-02-12 09:41:37 +0000

    Added hr to bin

--- mksymlink.sh
+++ mksymlink.sh
@@ -16,6 +16,20 @@ FILE_LIST="bashrc profile vimrc vim gvimrc bash_aliases bash_functions inputrc
 bash_profile wgetrc tmux.conf gitconfig gitignore"
 cd $HOME
 for file in $FILE_LIST; do
-    mv .$file $CONFIGDIR/$OLDFOLDER
-    ln -s $CONFIGDIR/$NEWFOLDER/$file .$file
+    diff $CONFIGDIR/$NEWFOLDER/$file .$file > /dev/null 2>&1
+    isdiff=$?
+    if [[ $isdiff -eq 1 ]]; then
+        mv .$file $CONFIGDIR/$OLDFOLDER
+        ln -s $CONFIGDIR/$NEWFOLDER/$file .$file
+    fi
 done
+cd -
+
+cd ~/bin
+for file in $CONFIGDIR/$NEWFOLDER/bin/*; do
+    linkfile=`basename $file`
+    if [ ! -f $linkfile ]]; then
+        ln -s $file $linkfile
+    fi
+done
+cd -
