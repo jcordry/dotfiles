ca09ef1c75a6014113937c99c883cd9435879aa9
Julien Cordry <julien.cordry@gmail.com>
2014-04-15 22:45:26 +0100

    nth works

--- bin/nth.sh
+++ bin/nth.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 word=$1
+first=$2
+second=$3
+third=$4
 
-echo ${word:$2:1} ${word:$3:1}  ${word:$4:1} 
+first=$((first-1))
+second=$((second-1))
+third=$((third-1))
+
+echo ${word:$first:1} ${word:$second:1} ${word:$third:1}
