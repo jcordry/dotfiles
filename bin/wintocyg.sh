ae7b1448e61279ef1a7caf91c0fb30b78d980c6b
Julien Cordry <julien.cordry@gmail.com>
2014-04-16 13:25:29 +0100

    Added convertion between windows path and cygwin path

---
+++ bin/wintocyg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: wintocyg.sh
+# 
+#         USAGE: ./wintocyg.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 16/04/2014 13:02
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+path=`echo $1 | tr '\' '/'`
+
+echo "$path"
+
+letter=`echo $path | sed -e 's/:\/.*//g'` # Should be the first letter!
+
+echo letter $letter
+
+if [[ "$letter" != "" ]]; then
+    path=`echo $path | sed -e 's/^.:\///'`
+    path="/cygdrive/`echo $letter | tr '[:upper:]' '[:lower:]'`/$path"
+fi
+
+echo "$path"
