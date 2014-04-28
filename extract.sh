28497d30822a5c57015bfc57904cadcbcaff2357
Julien Cordry <julien.cordry@gmail.com>
2014-04-28 08:18:52 +0100

    Recursive update in submodules

--- extract.sh
+++ extract.sh
@@ -2,4 +2,4 @@
 
 # Extract all submodules (from vim/).
 
-git pull origin master && git submodule init && git submodule update && git submodule status
+git pull origin master && git submodule init && git submodule update --recursive && git submodule status
