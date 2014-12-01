eb09b31fd5bbf8be6491afb851e87c517a83c966
Julien Cordry <julien.cordry@gmail.com>
2014-12-01 15:17:25 +0000

    updates git now

--- bin/update.sh
+++ bin/update.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-LECTURES=$HOME/lectures
+lectures=$HOME/lectures
 
-SVNARCHIVES= #"$LECTURES/mgp $LECTURES/ncp $LECTURES/mob"
-# GITARCHIVES="$LECTURES/enp $HOME/dotfiles"
-GITARCHIVES=$HOME/dotfiles
+# svnarchives= #"$lectures/mgp $lectures/ncp $lectures/mob"
+# gitarchives="$lectures/enp $HOME/dotfiles"
+gitarchives="$HOME/dotfiles $lectures/ncp.git $lectures/msg.git $lectures/mgd.git $lectures/ocaml"
 
-for ARCH in $SVNARCHIVES; do
-    svn up $ARCH
+for arch in $svnarchives; do
+    svn up $arch
 done
 
-for ARCH in $GITARCHIVES; do
-    pushd $ARCH
+for arch in $gitarchives; do
+    pushd $arch
     git pull origin master
     popd
 done
