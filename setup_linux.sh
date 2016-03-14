5f5b0ebd0b7dba274baa9211188883e443adf497
Julien Cordry <u0026939@linux21.scm.tees.ac.uk>
2016-03-14 13:34:53 +0000

    updated to the latest homespace

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+export NETHOME=/nethome1/scm-staff
+
 if [ -d .config ]; then
   rm -r .config
 fi
 
 ln -s $NETHOME/$USER nethome
-for dir in bin include .config .fonts dotfiles lib etc .emacsd; do
+for dir in .ssh bin include .config .fonts dotfiles lib lib64 Documents etc .emacs.d; do
   ln -s $NETHOME/$USER/$dir $dir
 done
 cd $HOME/dotfiles
